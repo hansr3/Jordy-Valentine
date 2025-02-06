52bd4a25e8adcf2519ed2c11a0f6cfb434f79a87
Hans <htja0002@student.monash.edu>
2025-02-06 14:40:14 +1100

    js initial handle click with error

---
+++ script.js
@@ -0,0 +1,15 @@
+let yes = document.getElementById('yes');
+let no = document.getElementById('no');
+let question = document.getElementById('question');
+
+const handleClickYes = () => {
+    // question.innerText = "WOOHOO, I love you POOKIE! ❤️🥳";
+    // document.body.appendChild(question);
+    console.log("you clicked yes");
+}
+
+const handleClickNo = () => {
+
+}
+
+yes.addEventListener('click', handleClickYes);
